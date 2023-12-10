e3dfff739eccfc41bb4fa2689c0ea4e9e07b4823
Oak <minkojee@gmail.com>
2023-12-11 00:45:10 +0700

    fixed filter UI

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -51,7 +51,7 @@ const SearchManufacturer = ({
 						leaveFrom="opacity-100"
 						leaveTo="opacity-0"
 						afterLeave={() => setQuery("")}>
-						<Combobox.Options>
+						<Combobox.Options className="absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
 							{filteredManufacturers.length === 0 && query !== "" ? (
 								<Combobox.Option
 									value={query}
